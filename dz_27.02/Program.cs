a4ba879750cf6098e2df59a91403e115027d510c
Sarp_dg92@student.itstep.org <Sarp_dg92@student.itstep.org>
2023-02-28 17:42:32 +0200

    Add project files.

---
+++ dz_27.02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_27._02
+{
+    class Program
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T buf = a;
+            a = b;
+            b = buf;
+        }
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Swap(ref a, ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
